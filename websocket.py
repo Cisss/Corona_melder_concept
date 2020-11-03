55c860a643fbb7a2d9a6a3c3aa4441b29421e0f1
Francisca van der Want <f.van.der.want@gmail.com>
2020-11-03 16:24:08 +0100

    improvements

--- websocket.py
+++ websocket.py
@@ -6,6 +6,7 @@ import json
 clients = []
 roles = {}
 rooms = {}
+players = {}
 class SimpleChat(WebSocket):
 
     def handleMessage(self):
@@ -17,28 +18,15 @@ class SimpleChat(WebSocket):
                 room = self.random_room()
                 self.sendMessage("room:" + str(room))
             elif message['request'] == 'question':
-                print('request for a message')
-                if rooms != {}:
-                    print('room is not empty')
-                    for room in rooms:
-                        print('foreach room')
-                        print(room)
-                        print(rooms[room])
-                        if self in rooms[room]:
-                            for client in clients:
-                                if client in rooms[room]:
-                                    client.sendMessage("random question")
-                            
-
+                self.random_player().sendMessage("random question")               
         # Add player to room
         elif 'join' in message:
-            room_number = message['join']
-            if room_number in rooms:
-                print("in hereee")
+            room = message['join']
+            if room in rooms:
                 datad = {self}
-                rooms[room_number].update(datad)
-                print('did just that')
-                print(rooms)
+                rooms[room] += datad
+                room_data = rooms[room]
+                players[room] = room_data
             else:
                 print('invalid number')
 
@@ -48,10 +36,35 @@ class SimpleChat(WebSocket):
         if room in rooms:
             self.random_room()
         else:
-            rooms[room] = {self}
-            print(rooms)
+            rooms[room] = [self]
             return room
 
+    # choose a random user
+    def random_player(self):
+        print("rooms", rooms)
+        # if there are one or more rooms
+        if rooms != []:
+            # select room that the current user is in
+            for room in rooms:
+                print(rooms[room])
+                if self in rooms[room]:
+                    # choose random player
+                    num = len(players[room])
+                    print("number", num)
+                    index = random.choice(range(num))
+                    active_player = players[room][index]
+                    # Remove player from players
+                    players[room].pop(index)
+                    # if there is only one player left reset players
+                    if(num == 1):
+                        print(players[room])
+                        print(rooms[room])
+                        players[room] = rooms[room]
+                        print(players[room])
+                    # return choosen user
+                    return active_player
+                        
+        
     def handleConnected(self):
         print(self.address, 'connected')
         for client in clients:
@@ -66,61 +79,3 @@ class SimpleChat(WebSocket):
 
 server = SimpleWebSocketServer('', 8000, SimpleChat)
 server.serveforever()
-
-
-
-
-
-
-#  if roles[self]:
-#     if roles[self] == 'host':
-#         for client in clients:
-#             if client == self:
-#                 room = json.loads(self.data)['room']
-#                 rooms[room] = self
-#                 print(rooms)
-#                 client.sendMessage(True, room)
-# else:
-#     client.sendMessage(False, "Kies eerst een role")
-
-
-
-# from flask import Flask, render_template
-# from flask_socketio import SocketIO, send, emit, join_room, leave_room
-    
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'secret!'
-# socketio = SocketIO(app)
-
-# @app.route('/')
-# def index():
-#     return 'hooi'
-
-# @socketio.on('connect')
-# def test_message():
-#    print("this works")
-#    emit('my response', {'data': 'Connected'})
-   
-
-# @socketio.on('join')
-# def on_join(data):
-#     username = data['username']
-#     room = data['room']
-#     join_room(room)
-#     send(username + ' has entered the room.', room=room)
-
-# @socketio.on('leave')
-# def on_leave(data):
-#     username = data['username']
-#     room = data['room']
-#     leave_room(room)
-#     send(username + ' has left the room.', room=room)
-
-# @socketio.on_error_default
-# def default_error_handler(e):
-#     print(request.event["message"]) # "my error event"
-#     print(request.event["args"])    # (data,)
-
-# if __name__ == '__main__':
-#     socketio.run(app)
-
